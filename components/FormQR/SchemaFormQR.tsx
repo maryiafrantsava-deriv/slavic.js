3225282d9391bc791a9bbaf58b0f6181acd8b6cb
maryiafrantsava-deriv <maryia.frantsava@deriv.com>
2022-01-27 18:56:46 +0300

    MaryiaF/feat: implement a form for QRCode and with help Formik getting the data object from inputs

---
+++ components/FormQR/SchemaFormQR.tsx
@@ -0,0 +1,18 @@
+import * as Yup from "yup";
+
+export const initialValuesRegister = {
+    firstName: "",
+    lastName: "",
+    homePhone: "",
+    workPhone: "",
+    workFax: "",
+    workEmail: "",
+    organization: "",
+    role: "",
+    street: "",
+    city: "",
+    zip: "",
+    state: "",
+    country: "",
+    website: "",
+}
