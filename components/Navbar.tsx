d0b88848ce62edc6d207fc7ba7f71d77344e0529
maryiafrantsava-deriv <maryia.frantsava@deriv.com>
2022-01-28 15:11:26 +0300

    MaryiaF/fix: vercel

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { useRouter } from "next/router";
-import Link from "next/Link";
+import Link from "next/link";
 import Image from 'next/image';
 import styles from '../styles/Navbar.module.scss';
 
